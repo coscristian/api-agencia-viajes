4eff06149617dbd2bf3abc5bb967e93761876ded
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-04-16 15:30:21 -0500

    Created spring boot project

---
+++ src/test/java/com/agencia/viajes/agencia/AgenciaViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agencia.viajes.agencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
