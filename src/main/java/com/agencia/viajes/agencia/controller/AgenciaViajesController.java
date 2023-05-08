624b3096097e940f90059045889ec676ca780f84
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-05-07 20:51:02 -0500

    Controlador rutas

--- src/main/java/com/agencia/viajes/agencia/controller/AgenciaViajesController.java
+++ src/main/java/com/agencia/viajes/agencia/controller/AgenciaViajesController.java
@@ -8,19 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.agencia.viajes.agencia.controller.dto.AdminGeneralDto;
-import com.agencia.viajes.agencia.controller.dto.SucursalDto;
 import com.agencia.viajes.agencia.controller.dto.TrabajoVendedorDto;
 import com.agencia.viajes.agencia.controller.dto.TuristaDto;
 import com.agencia.viajes.agencia.controller.dto.VendedorDto;
-import com.agencia.viajes.agencia.service.AdminGeneralService;
-import com.agencia.viajes.agencia.service.SucursalService;
 import com.agencia.viajes.agencia.service.TrabajoVendedorService;
 import com.agencia.viajes.agencia.service.TuristaService;
 import com.agencia.viajes.agencia.service.VendedorService;
@@ -43,8 +37,8 @@ import lombok.AllArgsConstructor;
 public class AgenciaViajesController { 
 
     private TuristaService turistaService;
-    private SucursalService sucursalService;
-    private AdminGeneralService adminGeneralService;
+    //private SucursalService sucursalService;
+    //private AdminGeneralService adminGeneralService;
     private VendedorService vendedorService;
     private TrabajoVendedorService trabajoVendedorService;
 
@@ -53,6 +47,7 @@ public class AgenciaViajesController {
         return turistaService.getTuristas();
     }
 
+    /*
     @GetMapping("/sucursales") // Petición de tipo GET a esta ruta
     public List<SucursalDto> getAllSucursales(){
         return sucursalService.getSucursales();
@@ -62,70 +57,32 @@ public class AgenciaViajesController {
     public List<AdminGeneralDto> getAllAdminsGeneral(){
         return adminGeneralService.getAdminsGeneral();
     }
+    */
 
-    @GetMapping("/vendedores") // Petición de tipo GET a esta ruta
+    @GetMapping("/vendedores") 
     public List<VendedorDto> getAllVendedores(){
         return vendedorService.getVendedores();
     }
-/*
-    @GetMapping("/vendedores")
-    public List<AdminGeneralDto> getAllVendedores(){
-        return
-    }
-    */
-    /*
-    @GetMapping("/sucursales")
-    public List<SucursalDto> getAllSucursales(){
-        return this.sucursales;
-    }
-
-    @GetMapping("/hoteles")
-    public List<HotelDto> getAllHoteles(){
-        return this.hoteles;
-    }
-
-    @GetMapping("/sucursal_admins")
-    public List<SucursalAdminDto> getAllSucursalAdmins(){
-        return this.sucursalAdmins;
-    }
-
-    @GetMapping("/vendedores")
-    public List<VendedorDto> getAllVendedores(){
-        return this.vendedores;
-    }
-    */
 
     // Model attribute permite recibir en ese objeto los datos que vienen en el body
     @PostMapping("/turista/crear")
     public ResponseEntity<String> postCrearTurista(@RequestBody TuristaDto turista){
-        System.out.println(turista.toString());
-        //System.out.println(turista.getNombre());
         turistaService.saveTurista(turista);
         return new ResponseEntity<>("Well done!!", HttpStatus.OK);
-
     }
 
     @PostMapping("/vendedor/crear")
     public ResponseEntity<String> postCrearVendedor(@RequestBody VendedorDto vendedor){
-        
-
-        //vendedor.setFechaAsignacion(dtf.format(now));
-        System.out.println(vendedor.toString());
-        
-        //System.out.println(turista.getNombre());
-        //turistaService.saveTurista(turista);
         vendedorService.saveVendedor(vendedor);
-
-
         return new ResponseEntity<>("Well done!!", HttpStatus.OK);
-
     }
 
+    /*
     @GetMapping("/trabajo_vendedor") // Petición de tipo GET a esta ruta
     public List<TrabajoVendedorDto> getTrabajoVendedor(){
-        //return vendedorService.getVendedores();
         return null;
     }
+    */
 
     @PostMapping("/trabajoVendedor/crear")
     public ResponseEntity<String> crearTrabajoVendedor(@RequestBody TrabajoVendedorDto trabajoVendedorDto){
@@ -138,13 +95,12 @@ public class AgenciaViajesController {
         return new ResponseEntity<>("Well done!!", HttpStatus.OK);
     }
 
-
+/*
     @GetMapping("/turista/{cod}")
     public String getTuristaById(@PathVariable("cod") String cod){
         var turistaOp = this.turistaService.getTuristaById(cod);
         if(turistaOp.isEmpty()) return "Error: El Turista con ID:" + cod + " no existe.";
         return turistaOp.get().getNombre();
     }
-    
-
+    */ 
 }
